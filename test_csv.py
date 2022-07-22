30405205899eb821e85c6ea92f9877843467e75e
Ari Gaskins <arigaskins.tech@gmail.com>
2022-07-22 18:40:50 -0400

    precision handling

--- test_csv.py
+++ test_csv.py
@@ -5,8 +5,8 @@ date_in = input('Enter the date: ')
 date_out = datetime.strptime(date_in, '%m/%d/%Y')
 date_out = date_out.date()
 gross_income = input('Enter gross income: $')
-gross_income = int(gross_income)
-tax = gross_income * 0.153
+gross_income = round(int(gross_income), 2)
+tax = round(gross_income * 0.153, 2)
 total_income = gross_income - tax
 
 print(f'The date is: {date_out}, the gross income is: {gross_income}, the tax is: {tax}, and the total income is: {total_income}.')
