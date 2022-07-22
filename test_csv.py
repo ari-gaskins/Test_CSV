24f122dcb64f05429a2e6e232238b24dc0f348ae
Ari Gaskins <arigaskins.tech@gmail.com>
2022-07-22 19:52:40 -0400

    create csv; test data adding

--- test_csv.py
+++ test_csv.py
@@ -1,6 +1,7 @@
-# import csv
+import csv
 from datetime import datetime, date
 
+# create variables for data inputs
 date_in = input('Enter the date: ')
 date_out = datetime.strptime(date_in, '%m/%d/%Y')
 date_out = date_out.date()
@@ -9,6 +10,22 @@ gross_income = round(int(gross_income), 2)
 tax = round(gross_income * 0.153, 2)
 total_income = gross_income - tax
 
-print(f'The date is: {date_out}, the gross income is: {gross_income}, the tax is: {tax}, and the total income is: {total_income}.')
+#print(f'The date is: {date_out}, the gross income is: {gross_income}, the tax is: {tax}, and the total income is: {total_income}.')
 
+# set csv header
+header = ['Date', 'Gross Income', 'Tax', 'Total Income']
 
+# set csv data
+data = [
+    [date_out, gross_income, tax, total_income]
+]
+
+# open csv
+with open('income.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write header to csv
+    writer.writerow(header)
+
+    # write data to csv
+    writer.writerow(data)
